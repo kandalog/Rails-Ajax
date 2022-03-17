af35d28268532fc5f2971f1e68726cfa31bd287e
kandalog <asuasu.tee@gmail.com>
2022-03-17 15:15:12 +0900

    更新機能作成にとりかかる

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: %i[destroy edit]
   def index
     @messages = Message.all
   end
@@ -7,11 +8,28 @@ class MessagesController < ApplicationController
     @message = Message.create!(message_params)
   end
 
+  def edit
+    
+  end
+
+  def destroy
+    @message.destroy!
+
+    #redirect_to root_url, status: :see_other
+
+  end
+
+  
+
 
   private
 
   def message_params
     params.require(:message).permit(:content)
+  end
+
+  def set_message
+    @message = Message.find(params[:id])
   end
 
 end
